9ec6ef99752ddefbf1a11f8c78ddf68d3bb81873
naimd66 <153229301+naimd66@users.noreply.github.com>
2025-06-15 18:06:14 +0200

    .

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,23 +1,65 @@
-resource "azurerm_linux_virtual_machine" "azure-vm" {
-  name                = "iac-vm-azure"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-  admin_username      = "testuser"
-  network_interface_ids = [azurerm_network_interface.nic.id]
+provider "azurerm" {
+  features {}
+  subscription_id = "c064671c-8f74-4fec-b088-b53c568245eb"
+  resource_provider_registrations = "none"
+}
 
-  disable_password_authentication = true
+resource "azurerm_virtual_network" "vnet" {
+  name                = "iac-vnet"
+  location            = "westeurope"
+  resource_group_name = "S1202816"
+
+  address_space = [
+    "10.0.0.0/16"
+  ]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "iac-nic"
+  location            = "westeurope"
+  resource_group_name = "S1202816"
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = "/subscriptions/c064671c-8f74-4fec-b088-b53c568245eb/resourceGroups/S1202816/providers/Microsoft.Network/virtualNetwor>    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "iac-pip"
+  location            = "westeurope"
+  resource_group_name = "S1202816"
+  allocation_method   = "Static"
+  sku                 = "Basic"
+}
+
+resource "azurerm_linux_virtual_machine" "azure-vm" {
+  name                  = var.vm_name
+  location              = "westeurope"
+  resource_group_name   = "S1202816"
+network_interface_ids = [azurerm_network_interface.nic.id]
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+
   admin_ssh_key {
-    username   = "testuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    username   = var.admin_username
+    public_key = file(var.public_key_path)
+  }
+
+  os_disk {
+    name              = "${var.vm_name}-osdisk"
+    caching           = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
-    sku       = "20_04-lts"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 
-  custom_data = filebase64("cloud-init.yaml")
+  disable_password_authentication = true
+  computer_name                   = var.vm_name
 }
